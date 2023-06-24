d03d65750cab17d19ec3212ff1041ca1c83bb149
Ken Tandrian <60643640+KenTandrian@users.noreply.github.com>
2023-06-24 11:50:56 +0000

    feat: add env example

--- src/lib/db-util.ts
+++ src/lib/db-util.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
 export const connectToDatabase = async () => {
-    const client = await MongoClient.connect(process.env.MONGODB_URI);
+    const client = await MongoClient.connect(process.env.MONGODB_URI!);
     return client;
 }
