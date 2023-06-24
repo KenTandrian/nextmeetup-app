fc7b721d9e012bd9fdc325ea8977a7bc51351122
Ken Tandrian <60643640+KenTandrian@users.noreply.github.com>
2023-06-24 11:43:39 +0000

    fix: add typing in next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     images: {
         domains: ['upload.wikimedia.org'],
     },
+    reactStrictMode: true,
+    swcMinify: true,
 }
+
+module.exports = nextConfig;
