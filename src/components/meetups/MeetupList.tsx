10c188a744b01249a2efd459fc98c7d8054e174c
Ken Tandrian <60643640+KenTandrian@users.noreply.github.com>
2023-08-29 05:06:22 +0000

    fix(ts): add missing types @ meetup list component

--- src/components/meetups/MeetupList.tsx
+++ src/components/meetups/MeetupList.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import MeetupItem from './MeetupItem';
 import classes from './MeetupList.module.css';
 
-function MeetupList(props) {
+function MeetupList(props: { meetups: Omit<IMeetup, "description">[] }) {
   return (
     <ul className={classes.list}>
       {props.meetups.map((meetup) => (
