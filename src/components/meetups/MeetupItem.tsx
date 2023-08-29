2147a42d538db8228d53be3a112c38c936e8edc9
Ken Tandrian <60643640+KenTandrian@users.noreply.github.com>
2023-08-29 05:09:19 +0000

    fix(ts): add missing types @ meetup item component

--- src/components/meetups/MeetupItem.tsx
+++ src/components/meetups/MeetupItem.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
-import Link from 'next/link';
+// import Link from 'next/link';
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
 import Image from 'next/image';
 
-function MeetupItem(props) {
+function MeetupItem(props: Omit<IMeetup, "description">) {
   const router = useRouter();
 
   const showDetailsHandler = () => {
